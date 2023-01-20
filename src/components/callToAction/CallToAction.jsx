52675feb77a39ae2890812236fe02bb7d4b840ed
Byaxy <charlesbyaxy@gmail.com>
2023-01-20 11:35:57 +0000

    gpt-3 website

---
+++ src/components/callToAction/CallToAction.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./callToAction.css";
+
+const CallToAction = () => {
+  return (
+    <div className="gpt3__callToAction section__margin">
+      <div className="gpt3__callToAction-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__callToAction-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
